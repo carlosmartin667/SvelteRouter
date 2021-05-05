748541393f042677247f93c3f7993ab5a9848314
carlosmartin moscosocornejo <carlosmartinmoscosocornejo@gmail.com>
2021-05-04 22:25:54 -0300

    creando vista de usuarios

--- svelteRutas/src/App.svelte
+++ svelteRutas/src/App.svelte
@@ -1,30 +1,35 @@
 <script>
-	export let name;
+  import { Router, Link, Route } from "svelte-routing";
+  import Contacto from "./views/contacto.svelte";
+  import Detalle from "./views/detalle.svelte";
+  import Home from "./views/home.svelte";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div>
+	<h1>App</h1>
+	<Router>
+		<nav>
+			<Link to="/">Home</Link>
+			<Link to="/Detalle">Detalle</Link>
+			<Link to="/Contacto">Contacto</Link>
+		</nav>
 
+		<Route path="/">
+			<Home></Home>
+		</Route>
+		<Route path="/Detalle">
+			<Detalle></Detalle>
+		</Route>
+		<Route path="/Contacto">
+			<Contacto></Contacto>
+		</Route>
+	</Router>
+</div>
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  h1 {
+    color: #00fff2;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 </style>
---
+++ svelteRutas/src/views/contacto.svelte
@@ -0,0 +1,4 @@
+<script>
+
+</script>
+<h1>hola  esto es contacto </h1>
